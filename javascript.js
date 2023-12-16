a580cd409881bb43e281b1b4f929020962c59d0a
Peaches-mp4 <131920771+Peaches-mp4@users.noreply.github.com>
2023-12-16 13:33:50 +0100

    Connect DOM elements to function

--- javascript.js
+++ javascript.js
@@ -2,18 +2,23 @@
 //define variables
 
 const myLibrary = [];
-const bookInput = document.getElementById('bookInput');
-const submitBtn = document.getElementById('submitBtn');
+const bookAuthor = document.getElementById('author');
+const bookTitle = document.getElementById('title');
+const bookPages = document.getElementById('pages-num');
+const bookLang = document.getElementById('language');
+const bookStatus = document.getElementById('read-status');
+const submitBtn = document.getElementById('submit-btn');
 const libraryDiv = document.getElementById('libraryDiv');
 //constructor
 
-function Book(title, author, pages, status) {
+function Book(title, author, pages, language, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.language = language;
     this.status = status;
     this.info = function() {
-        return title + ' by ' + author + ', ' + pages + ' pages, ' + status;
+        return title + ' by ' + author + ', ' + pages + ' pages, ' + 'written in ' + language + ' language, ' + status;
     }
 };
 
@@ -22,10 +27,10 @@ submitBtn.addEventListener('click', addBookToLibrary);
 
 //Add user books to arr
 
-function addBookToLibrary() {
-    console.log(bookInput.value);
+function addBookToLibrary(event) {
+    event.preventDefault();
 
-    myLibrary.push(new Book(bookInput.value));
+    myLibrary.push(new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookLang.value, bookStatus.value));
     console.log(myLibrary);
     loopAndDisplay();
 }
@@ -47,8 +52,4 @@ function loopAndDisplay() {
     libraryDiv.appendChild(newDiv);
 }
 
-
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
-
-console.log(theHobbit.info());
 
